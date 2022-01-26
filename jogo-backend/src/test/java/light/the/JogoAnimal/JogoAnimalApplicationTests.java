9ae4d8c9e3338aea7a06d7d31c2af67efbcee1fe
LightThe <gallotheo@icloud.com>
2022-01-25 22:09:52 -0300

    Banco de dados inicial, estrutura do projeto

---
+++ jogo-backend/src/test/java/light/the/JogoAnimal/JogoAnimalApplicationTests.java
@@ -0,0 +1,13 @@
+package light.the.JogoAnimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogoAnimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
